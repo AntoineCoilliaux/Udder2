7af09cc8b32773178b54736373038074eec04021
Antoine Coilliaux <antoine.coilliaux@gmail.com>
2025-04-23 09:53:37 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Antoine Coilliaux on 23/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
